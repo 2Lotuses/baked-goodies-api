23b2095079579ba5db0590fe4ddf355578d1023b
NinoPogi <ninoimmanoelbautista@gmail.com>
2023-04-15 12:14:48 +0800

    Shorter Routes

--- misc/db.js
+++ misc/db.js
@@ -4,5 +4,5 @@ import logger from "./logger.js";
 dotenv.config();
 export default function () {
   const db = process.env.db;
-  mongoose.connect(db).then(() => logger.info(`Connected to ${db}...`));
+  mongoose.connect(db).then(() => logger.info(`Connected to the database...`));
 }
--- misc/routes.js
+++ misc/routes.js
@@ -2,13 +2,9 @@ import express from "express";
 import error from "../middleware/error.js";
 import order from "../routes/order.js";
 import upload from "../routes/upload.js";
-import helmet from "helmet";
-import morgan from "morgan";
 export default function (app) {
-  app.use(helmet());
   app.use(express.json({ extended: false }));
-  app.use("/api/order", order);
-  app.use("/api/upload", upload);
+  app.use("/order", order);
+  app.use("/upload", upload);
   app.use(error);
-  app.use(morgan);
 }
