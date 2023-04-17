b32db17ce2f95c92ba38239b4d083cf606e76772
NinoPogi <ninoimmanoelbautista@gmail.com>
2023-04-17 16:02:34 +0800

    Added Cors

--- misc/routes.js
+++ misc/routes.js
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import Order from "../routes/order.js";
 import Upload from "../routes/upload.js";
 import Customer from "../routes/customer.js";
 
 export default function (app) {
+  app.use(
+    cors({
+      origin: "https://baked-goodies.vercel.app/",
+    })
+  );
   app.use(express.json({ extended: false }));
   app.use("/customer", Customer);
   app.use("/order", Order);
