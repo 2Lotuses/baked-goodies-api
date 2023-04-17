76f89e27e39b17d72a58bafd10ff9fee28d8b638
NinoPogi <ninoimmanoelbautista@gmail.com>
2023-04-17 17:59:05 +0800

    Change Multer to MemoryStorage

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,9 @@
 import express from "express";
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
 import * as dotenv from "dotenv";
+import DatauriParser from "datauri/parser.js";
+import path from "path";
 
 dotenv.config();
 
@@ -15,32 +16,27 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
-const storage = multer.diskStorage({
-  destination: (req, file, callBack) => {
-    callBack(null, "./uploads");
-  },
-  filename: (req, file, callBack) => {
-    callBack(null, `BG ${file.originalname}`);
-  },
-});
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const upload = multer({ storage: storage });
+const parser = new DatauriParser();
 
 router.post("/", upload.array("imageUpload", 5), async (req, res) => {
   if (req.files === []) {
     return res.status(400).send("No file were uploaded");
   }
-
-  const imageArray = [];
+  const linkArray = [];
 
   for (let file of req.files) {
-    const result = await cloudinary.uploader.upload(`./${file.path}`);
-    imageArray.push(result.secure_url);
-
-    fs.unlinkSync(`./${file.path}`);
+    const file64 = parser.format(
+      path.extname(file.originalname).toString(),
+      file.buffer
+    );
+    const result = await cloudinary.uploader.upload(file64.content);
+    linkArray.push(result.url);
   }
 
-  res.send(imageArray);
+  res.send(linkArray);
 });
 
 export default router;
